9cfc28e5c665d6a8835afbeff358b8bf531be3cf
MaxJGruber <maxjgruber@gmail.com>
2020-09-28 18:56:11 +0200

    CSS brushups on all forms

--- app.js
+++ app.js
@@ -30,13 +30,13 @@ app.use(express.static(path.join(__dirname, "public")));
 hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 app.use(
-    session({
-        store: new MongoStore({ mongooseConnection: mongoose.connection }),
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        cookie: {},
-    })
+  session({
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {},
+  })
 );
 
 // app.use(require("./middleware/protectAdminRoute"))
@@ -48,21 +48,19 @@ app.use("/", usersAdminRouter);
 app.use("/games", gamesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-
-
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
