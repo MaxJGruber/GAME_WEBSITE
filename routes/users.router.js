4e8c7409763a7794e5adb038e128ba216ae9ff87
MaxJGruber <maxjgruber@gmail.com>
2020-09-28 15:30:04 +0200

    Form for edit user

--- routes/users.router.js
+++ routes/users.router.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const UserModel = require("../models/User.model");
 const bcrypt = require("bcrypt");
 
-
 router.get("/user/:id/edit", async (req, res, next) => {
+  try {
+    res.render("manage_games");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/user/:id/edit", async (req, res, next) => {
   try {
     const updatedUser = await UserModel.findByIdAndUpdate(
       req.params.id,
