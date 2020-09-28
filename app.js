08a4ec75c2676550c10a89e275850e89ff712dd4
vincentpro <vincent_pro@hotmail.fr>
2020-09-28 16:46:08 +0200

    added route in app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const indexRouter = require("./routes/home.router");
 const usersRouter = require("./routes/users.router");
+const usersAdminRouter = require("./routes/users.router");
 const gamesRouter = require("./routes/games");
 
 const app = express();
@@ -29,33 +30,34 @@ app.use(express.static(path.join(__dirname, "public")));
 hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 app.use(
-  session({
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {},
-  })
+    session({
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {},
+    })
 );
 
 app.use("/", indexRouter);
-app.use(usersRouter);
+app.use("/", usersRouter);
+app.use("/", usersAdminRouter);
 app.use("/games", gamesRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
