5eca3bcdd77ea0c338f290a95b11f49de964b271
MaxJGruber <maxjgruber@gmail.com>
2020-09-28 12:48:39 +0200

    Route for all games done

---
+++ routes/games.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+const Games = require("../models/Game");
+
+// GETTING ALL GAMES
+
+router.get("/collection", async function (req, res, next) {
+  try {
+    const dbResult = await Games.find();
+    res.render("collection", { games: dbResult });
+  } catch (error) {
+    next(error);
+  }
+});
