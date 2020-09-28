c5cdd8726dc461429fa00129bfe358a18f5e5870
Timothée Nicole <timothee.nicole@gmail.com>
2020-09-28 12:15:55 +0200

    user model created

---
+++ models/User.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  pseudo: String,
+  email: { type: String, unique: true },
+  password: String,
+  age: Number,
+  favConsole: {type: String, enum: [
+    "PlayStation 4",
+    "PlayStation 5",
+    "PlayStation 3",
+    "PlayStation 2",
+    "PlayStation",
+    "Xbox One",
+    "Xbox Series X",
+    "Xbox 360",
+    "PC",
+    "Steam",
+    "Mobile",
+    "PSP",
+    "PS Vita",
+    "Nintendo DS",
+    "Nintendo WII",
+    "Nintendo SWITCH",
+  ]},
+  favGenre: {type: String, enum: ["Action", "Indie", "Adventure","RPG", "Strategy", "Shooter", "Casual", "Simulation", "Puzzle", "Arcade", "Platformer", "Racing", "Sports", "Massively Multiplayer", "Family", "Fighting", "Board Games", "Educational", "Card"]}, // A completer
+  role: { type: String, enum: ["admin", "user"], default: "user" },
+  image: {type: String, default: "https://www.kindpng.com/picc/m/105-1055561_gaming-logo-avatar-png-transparent-png.png"},
+  wishlist: Array[Number], // [Schema.Types.ObjectID, ref: "Game"]
+  owned: Array[Number], // [Schema.Types.ObjectID, ref: "Game"]
+  finished: Array[Number], // [Schema.Types.ObjectID, ref: "Game"]
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
