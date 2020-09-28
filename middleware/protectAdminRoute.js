61e2331e0b9acfaa335a42aa85c3f4b3f38ae381
vincentpro <vincent_pro@hotmail.fr>
2020-09-28 15:44:18 +0200

    added admin user routes and view

---
+++ middleware/protectAdminRoute.js
@@ -0,0 +1,10 @@
+function protectedAdminRoute(req, res, next) {
+    if (req.session.currentUser && req.session.currentUser.role === "admin") {
+        next();
+    } else {
+        req.flash("error", "Sorry you do not have access to that page");
+        res.redirect("/");
+    }
+}
+
+module.exports = protectedAdminRoute;
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const UserModel = require("../models/User.model")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const protectedAdminRoute = require('../middleware/protectAdminRoute');
 
-
+//route to display all users to an admin
+router.get('/users', protectedAdminRoute, async function(req, res, next) {
+    const userlist = await UserModel.find();
+    res.render('users', { users: userlist });
+});
 
 
 module.exports = router;
