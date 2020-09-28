ad42abeb363ecb49f070a1c1aaa9817869b71f26
MaxJGruber <maxjgruber@gmail.com>
2020-09-28 15:52:13 +0200

    Merge conflicts

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 const UserModel = require("../models/User.model")
 const bcrypt = require("bcrypt");
 const protectedAdminRoute = require('../middleware/protectAdminRoute');
@@ -10,10 +9,6 @@ router.get('/users', protectedAdminRoute, async function(req, res, next) {
     const userlist = await UserModel.find();
     res.render('users', { users: userlist });
 });
-=======
-const UserModel = require("../models/User.model");
-const bcrypt = require("bcrypt");
->>>>>>> 128a3dc169a0029330d1169a3a3dc9ebe02b357e
 
 router.get("/user/:id/edit", async (req, res, next) => {
   try {
@@ -23,20 +18,4 @@ router.get("/user/:id/edit", async (req, res, next) => {
   }
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-router.post("/user/:id/edit", async (req, res, next) => {
-  try {
-    const updatedUser = await UserModel.findByIdAndUpdate(
-      req.params.id,
-      req.body
-    );
-    res.redirect("/");
-  } catch (err) {
-    next(err);
-  }
-});
-
-module.exports = router;
->>>>>>> 128a3dc169a0029330d1169a3a3dc9ebe02b357e
