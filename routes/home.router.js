254db1a1edb14c0ad69f96254ac98629145daaaf
Timothée Nicole <timothee.nicole@gmail.com>
2020-09-28 15:46:58 +0200

    commit before merge

--- routes/home.router.js
+++ routes/home.router.js
@@ -5,11 +5,13 @@ const bcrypt = require("bcrypt")
 
 const salt = 10
 /* GET home page. */
+
+// Render the hbs file for the homepage
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-
+// render the hbs file for signing up to the website
 router.get('/signup', (req, res, next) => {
   try {
     res.render('signup');
@@ -18,6 +20,7 @@ router.get('/signup', (req, res, next) => {
   }
 });
 
+// create User information for the database, from the form signup.hbs
 router.post("/signup", async (req, res, next) => {
   try {
 
@@ -37,6 +40,42 @@ router.post("/signup", async (req, res, next) => {
   } catch(err) {
     next(err)
   }
+});
+
+// render the hbs file for signing in to the website
+router.get("/signin", async (req, res, next) => {
+  try {
+    res.render("signin.hbs")
+  } catch(err) {
+    next(err)
+  }
+});
+
+// fetch database and document to make sur the user who wants to connect exists
+router.post("/signin", async (req, res, next) => {
+  try {
+    const {email, password } = req.body;
+    const foundUser = await UserModel.findOne({email: email})
+
+    if (!foundUser) {
+      res.redirect("signin", {error: "Invalid Credentials"})
+    } else {
+      const isSamePassword = bcrypt.compareSync(password, foundUser.password )
+      if (!isSamePassword) {
+        res.redirect("signin", {error: "Invalid Credentials"})
+      } else {
+        const userDocument = {...foundUser};
+        const userObject = foundUser.toObject();
+        delete userObject.password;
+        req.session.currentUser = userObject
+        res.redirect("/")
+      }
+    }
+  } catch(err) {
+    next(err)
+  }
 })
+
+
 
 module.exports = router;
