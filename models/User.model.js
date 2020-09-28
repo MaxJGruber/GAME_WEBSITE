22c251847cd14bf0d2a3324ccb71123c5004ed17
Timothée Nicole <timothee.nicole@gmail.com>
2020-09-28 19:12:21 +0200

    checkbox done

--- models/User.model.js
+++ models/User.model.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
   email: { type: String, unique: true },
   password: String,
   age: Number,
-  favConsole: {type: String, enum: [
+  favConsole: {type: [String], enum: [
     "PlayStation 4",
     "PlayStation 5",
     "PlayStation 3",
@@ -25,7 +25,7 @@ const userSchema = new Schema({
     "Nintendo WII",
     "Nintendo SWITCH",
   ]},
-  favGenre: {type: String, enum: ["Action", "Indie", "Adventure","RPG", "Strategy", "Shooter", "Casual", "Simulation", "Puzzle", "Arcade", "Platformer", "Racing", "Sports", "Massively Multiplayer", "Family", "Fighting", "Board Games", "Educational", "Card"]}, // A completer
+  favGenre: {type: [String], enum: ["Action", "Indie", "Adventure","RPG", "Strategy", "Shooter", "Casual", "Simulation", "Puzzle", "Arcade", "Platformer", "Racing", "Sports", "Massively Multiplayer", "Family", "Fighting", "Board Games", "Educational", "Card"]}, // A completer
   role: { type: String, enum: ["admin", "user"], default: "user" },
   image: {type: String, default: "https://www.kindpng.com/picc/m/105-1055561_gaming-logo-avatar-png-transparent-png.png"},
   wishlist: [Number], // [Schema.Types.ObjectID, ref: "Games"]
