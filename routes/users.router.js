e313a0998ab5f49110ef3f41f4c35e042680ac9a
Timothée Nicole <timothee.nicole@gmail.com>
2020-09-28 16:01:05 +0200

    commit before pull

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,19 +1,7 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-const UserModel = require("../models/User.model")
-const bcrypt = require("bcrypt");
-const protectedAdminRoute = require('../middleware/protectAdminRoute');
-
-//route to display all users to an admin
-router.get('/users', protectedAdminRoute, async function(req, res, next) {
-    const userlist = await UserModel.find();
-    res.render('users', { users: userlist });
-});
-=======
 const UserModel = require("../models/User.model");
 const bcrypt = require("bcrypt");
->>>>>>> 128a3dc169a0029330d1169a3a3dc9ebe02b357e
 
 router.get("/user/:id/edit", async (req, res, next) => {
   try {
@@ -23,9 +11,6 @@ router.get("/user/:id/edit", async (req, res, next) => {
   }
 });
 
-<<<<<<< HEAD
-module.exports = router;
-=======
 router.post("/user/:id/edit", async (req, res, next) => {
   try {
     const updatedUser = await UserModel.findByIdAndUpdate(
@@ -39,4 +24,3 @@ router.post("/user/:id/edit", async (req, res, next) => {
 });
 
 module.exports = router;
->>>>>>> 128a3dc169a0029330d1169a3a3dc9ebe02b357e
