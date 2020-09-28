d453ad4628fef91db6ea80abe78f97b2eac1eadb
vincentpro <vincent_pro@hotmail.fr>
2020-09-28 16:33:49 +0200

    fixed conflict2

--- routes/users.router.js
+++ routes/users.router.js
@@ -2,35 +2,25 @@ const express = require("express");
 const router = express.Router();
 const UserModel = require("../models/User.model");
 
-<<<<<<< HEAD
 router.get("/user/edit", async(req, res, next) => {
     try {
-        res.render("manage_games");
+        const user = await UserModel.findById(req.session.currentUser.id);
+        res.render("user", { user });
     } catch (err) {
         next(err);
     }
-=======
-router.get("/user/edit", async (req, res, next) => {
-  try {
-    const user = await UserModel.findById(req.session.currentUser.id);
-    res.render("user", { user });
-  } catch (err) {
-    next(err);
-  }
->>>>>>> 3d717b645dbb8adfebc4b8179cb07f562c558efc
 });
 
-router.post("/user/edit", async (req, res, next) => {
-  try {
-    const updatedUser = await UserModel.findByIdAndUpdate(
-      req.session.currentUser.id,
-      req.body,
-      { new: true }
-    );
-    res.redirect("/user/edit");
-  } catch (err) {
-    next(err);
-  }
+router.post("/user/edit", async(req, res, next) => {
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            req.session.currentUser.id,
+            req.body, { new: true }
+        );
+        res.redirect("/user/edit");
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
