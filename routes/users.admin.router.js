f667d6732a61f9926202bfa4e2785ecc11893df8
vincentpro <vincent_pro@hotmail.fr>
2020-09-28 16:11:43 +0200

    added user.hbs

---
+++ routes/users.admin.router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const UserModel = require("../models/User.model")
+const protectedAdminRoute = require('../middleware/protectAdminRoute');
+
+//route to display all users to an admin
+router.get('/users', protectedAdminRoute, async function(req, res, next) {
+    const userlist = await UserModel.find();
+    res.render('users', { users: userlist });
+});
+
+//route to display all users to an admin
+router.get('/users/:id', protectedAdminRoute, async function(req, res, next) {
+    const user = await UserModel.findById(req.params.id);
+    res.render('user', { users: user });
+});
+
+//route to display all users to an admin
+router.post('/users/:id', protectedAdminRoute, async function(req, res, next) {
+    await UserModel.findByIdAndUpdate(req.params.id);
+    res.redirect('/users');
+});
+
+//route to display all users to an admin
+router.delete('/users/:id', protectedAdminRoute, async function(req, res, next) {
+    const userlist = await UserModel.findByIdAndDelete();
+    res.render('users', { users: userlist });
+});
