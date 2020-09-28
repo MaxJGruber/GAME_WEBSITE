c505f39178edb0556c3b98af7d37841d4dca9a44
MaxJGruber <maxjgruber@gmail.com>
2020-09-28 12:04:29 +0200

    Added views

--- models/Game.js
+++ models/Game.js
@@ -1,2 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+  rawgid: Number,
+  title: String,
+  image: String,
+  metacritic: Number,
+  genre: {
+    type: String,
+    enum: [
+      "action",
+      "horror",
+      "rpg",
+      "adventure",
+      "shooter",
+      "sports",
+      "education",
+      "family",
+      "fps",
+    ],
+  },
+  platform: {
+    type: String,
+    enum: [
+      "",
+      "horror",
+      "rpg",
+      "adventure",
+      "shooter",
+      "sports",
+      "education",
+      "family",
+      "fps",
+    ],
+  },
+});
+
+const Game = mongoose.model("Games", gameSchema);
+
+module.exports = Game;
