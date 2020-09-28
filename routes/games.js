0c03c5a4560f5d8dd947751e6291d3c4a8c7303b
vincentpro <vincent_pro@hotmail.fr>
2020-09-28 13:59:23 +0200

    tesing lauching app

--- routes/games.js
+++ routes/games.js
@@ -4,11 +4,11 @@ const Games = require("../models/Game");
 
 // GETTING ALL GAMES
 
-router.get("/collection", async function (req, res, next) {
-  try {
-    const dbResult = await Games.find();
-    res.render("collection", { games: dbResult });
-  } catch (error) {
-    next(error);
-  }
+router.get("/collection", async function(req, res, next) {
+    try {
+        const dbResult = await Games.find();
+        res.render("collection", { games: dbResult });
+    } catch (error) {
+        next(error);
+    }
 });
