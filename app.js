b0358eb389473b316232a3e025b013f10f9cbebf
MaxJGruber <maxjgruber@gmail.com>
2020-09-28 12:54:46 +0200

    Merge conflicts

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 require("./config/mongodb");
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const hbs = require("hbs");
 const mongoose = require("mongoose");
 // const MongoStore = require("connect-mongo")(session);
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 const gamesRouter = require("./routes/games");
 
 const app = express();
