02b9fb15bbab361c8f4d7b423345afac74840948
MaxJGruber <maxjgruber@gmail.com>
2020-09-28 10:45:33 +0200

    Navbar collapse completed

---
+++ public/javascripts/navbarCollapse.js
@@ -0,0 +1,11 @@
+export function navbarCollapse() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+const collapse = document.querySelector(".icon");
+collapse.onclick = navbarCollapse;
