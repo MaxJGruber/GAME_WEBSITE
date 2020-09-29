73f9de42633794a6a42f33b77d28875d26141f1b
MaxJGruber <maxjgruber@gmail.com>
2020-09-29 11:38:38 +0200

    Rendering of signle game page complete

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const Games = require("../models/Game");
+const axios = require("axios");
 
 // GETTING ALL GAMES
 
@@ -18,8 +19,26 @@ router.get("/collection", async function (req, res, next) {
 router.get("/collection/game/:id", async function (req, res, next) {
   try {
     const selectedGame = await Games.findById(req.params.id);
+    const axiosResult = await axios.get(
+      `https://api.rawg.io/api/games/${selectedGame.rawgid}`
+    );
+    console.log(axiosResult.data.description);
     res.render("oneGame", {
       selectedGame,
+      description: axiosResult.data.description,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// ZOOM ON ONE SCREENSHOT
+
+router.get("/collection/screenshot/:id", async function (req, res, next) {
+  try {
+    const selectedScreenshot = await Games.findById(req.params.id);
+    res.render("screenshot-wide-screen", {
+      selectedScreenshot,
     });
   } catch (error) {
     next(error);
