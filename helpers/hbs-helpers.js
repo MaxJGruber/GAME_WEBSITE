a6c0957a208c33315c4c6b0fa536f71b8a1440f4
Timothée Nicole <timothee.nicole@gmail.com>
2020-09-29 11:20:36 +0200

    added files

---
+++ helpers/hbs-helpers.js
@@ -0,0 +1,7 @@
+hbs.registerHelper("isSameId", function (value1, value2, options) {
+    if (value1.toString() === value2.toString()) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
