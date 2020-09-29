bc97827b938e3cd85a6f7f36cca926511f0fdb80
MaxJGruber <maxjgruber@gmail.com>
2020-09-29 16:51:30 +0200

    Added mongo queries comments

---
+++ Json-template/mongo-queries.js
@@ -0,0 +1,20 @@
+//One genre AND 2 platforms
+// {$and: [{genres : "Action"}, {plateform : {$in : ["Xbox 360", "PC"]}}]}
+
+//One genre AND One platform
+// {$and: [{genres : "Action"}, {plateform : "Xbox 360"}]}
+
+//One genre AND NO platforms
+//{genres : "Shooter"}
+
+//NO genre and One platform
+//{plateforms : "Xbox 360"}
+
+//Two genres and NO platforms
+//{genres : ["Xbox 360", "PC"]}
+
+//NO genres AND Two platforms
+//{plateforms : ["Action", "Shooter"]}
+
+//NO FILTER
+//
