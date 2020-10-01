fac1d401a39e86c3035bb79af1d4bd242fe30314
MaxJGruber <maxjgruber@gmail.com>
2020-10-01 16:09:17 +0200

    brushing up CSS on one game display

--- public/javascripts/navbarCollapse.js
+++ public/javascripts/navbarCollapse.js
@@ -1,9 +1,9 @@
 export function navbarCollapse() {
   var navBar = document.getElementById("myTopnav");
   if (navBar.className === "topnav") {
-    navBar.className.add("responsive");
+    navBar.className += " responsive";
   } else {
-    navBar.className.remove("responsive");
+    navBar.className = "topnav";
   }
 }
 
