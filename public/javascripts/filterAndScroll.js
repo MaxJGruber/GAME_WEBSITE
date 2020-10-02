54f789230d0741e56cd169a559a00c1c26a75733
MaxJGruber <maxjgruber@gmail.com>
2020-10-02 14:48:44 +0200

    adding CSS

--- public/javascripts/filterAndScroll.js
+++ public/javascripts/filterAndScroll.js
@@ -15,76 +15,85 @@ platformChkbox.onclick = platformHandler;
 genreChkbox.onclick = genreHandler;
 
 function platformHandler() {
-    platformDiv.classList.toggle("hidden");
+  platformDiv.classList.toggle("hidden");
 }
 
 function genreHandler() {
-    genreDiv.classList.toggle("hidden");
+  genreDiv.classList.toggle("hidden");
 }
 
 platformDiv.onclick = (e) => e.stopPropagation();
+genreDiv.onclick = (e) => e.stopPropagation();
 
-genreBox.forEach(box => box.onclick = clickHandler);
-platformBox.forEach(box => box.onclick = clickHandler);
+genreBox.forEach((box) => (box.onclick = clickHandler));
+platformBox.forEach((box) => (box.onclick = clickHandler));
 searchBar.oninput = inputHandler;
 
 let page = 2;
 let typing;
 
 // Detect when scrolled to bottom.
-htmlBody.addEventListener('scroll', async function() {
-    //console.log(htmlBody.scrollTop + htmlBody.clientHeight, htmlBody.scrollHeight)
-    if (htmlBody.scrollTop + htmlBody.clientHeight >= htmlBody.scrollHeight - 1) {
-        filterHandler();
-    }
+htmlBody.addEventListener("scroll", async function () {
+  //console.log(htmlBody.scrollTop + htmlBody.clientHeight, htmlBody.scrollHeight)
+  if (htmlBody.scrollTop + htmlBody.clientHeight >= htmlBody.scrollHeight - 1) {
+    filterHandler();
+  }
 });
 
 function inputHandler() {
-    console.log("titi");
-    clearTimeout(typing);
-    typing = setTimeout(() => clickHandler(), 500);
+  console.log("titi");
+  clearTimeout(typing);
+  typing = setTimeout(() => clickHandler(), 500);
 }
 
 function clickHandler() {
-    page = 1; //reset index of page
-    htmlBody.innerHTML = '';
-    filterHandler();
+  page = 1; //reset index of page
+  htmlBody.innerHTML = "";
+  filterHandler();
 }
 
 async function filterHandler() {
-    console.log("filterhandler");
-    //get dom value
-    let selectedGenre = [];
-    let selectedPlatform = [];
-    let platformList = [];
-    let genreList = [];
+  console.log("filterhandler");
+  //get dom value
+  let selectedGenre = [];
+  let selectedPlatform = [];
+  let platformList = [];
+  let genreList = [];
 
-    let arrayList = [...genreBox];
-    genreList = arrayList.filter(input => input.checked); //array of selected input in genre
-    genreList.forEach(genre => selectedGenre.push(genre.id));
+  let arrayList = [...genreBox];
+  genreList = arrayList.filter((input) => input.checked); //array of selected input in genre
+  genreList.forEach((genre) => selectedGenre.push(genre.id));
 
-    arrayList = [...platformBox];
-    console.log(arrayList);
-    platformList = arrayList.filter(input => input.checked); //array of selected input in platform
-    platformList.forEach(platform => selectedPlatform.push(platform.id));
+  arrayList = [...platformBox];
+  console.log(arrayList);
+  platformList = arrayList.filter((input) => input.checked); //array of selected input in platform
+  platformList.forEach((platform) => selectedPlatform.push(platform.id));
 
-    let searchValue = searchBar.value; // value of search bar
-    console.log("loading game page ", page);
-    const gamesFiltered = await apiFilter.getFilterAndPAge(page, selectedGenre, selectedPlatform, searchValue);
-    page++;
-    console.log(gamesFiltered.data);
+  let searchValue = searchBar.value; // value of search bar
+  console.log("loading game page ", page);
+  const gamesFiltered = await apiFilter.getFilterAndPAge(
+    page,
+    selectedGenre,
+    selectedPlatform,
+    searchValue
+  );
+  page++;
+  console.log(gamesFiltered.data);
 
-    for (let game of gamesFiltered.data) {
-        createGameDiv(game);
-    }
+  for (let game of gamesFiltered.data) {
+    createGameDiv(game);
+  }
 }
 
 function createGameDiv(game) {
-    let div = document.createElement("div");
-    div.classList.add("game-container");
-    div.innerHTML = `<a href="/games/collection/game/${game._id}">
+  let div = document.createElement("div");
+  div.classList.add("game-alignment");
+  div.innerHTML = `<div class="game-container">
+    <a href="/games/collection/game/${game._id}" class="game-link">
         <h4 class="game-title">${game.title}</h4>
-        <img src="${game.img}" alt="${game.title}-poster" class="game-poster">        
+        <img src="${game.img}" alt="${
+    game.title
+  }-poster" class="game-poster">        
         <h5 class="genre">
             ${game.genres.join(" / ")}
         </h5>
@@ -92,18 +101,22 @@ function createGameDiv(game) {
             ${game.plateform.join(" / ")}
         </h5>
         <div class="add-btn">
-            <a class="addToCollec" data-id="${game._id}"><i class="far fa-plus-square"></i>  <span class="add">Add to Collection</span></a>
+            <a class="addToCollec" data-id="${
+              game._id
+            }"><i class="far fa-plus-square"></i>  <span class="add">Add to Collection</span></a>
         </div>
         <div class="add-btn">
-            <a class="addToFinish" data-id="${game._id}"><i class="far fa-check-square"></i>  <span class="add">Add to Finished Games</span></a>
+            <a class="addToFinish" data-id="${
+              game._id
+            }"><i class="far fa-check-square"></i>  <span class="add">Add to Finished Games</span></a>
         </div>
-        </a>`
-    htmlBody.appendChild(div);
+        </a></div>`;
+  htmlBody.appendChild(div);
 
-    // `<h4 class="game-title">${game.title}</h4>
-    // <img src="${game.img}" alt="${game.title}-poster" class="game-poster">
-    // <h5 class="genre">${game.genres}</h5>
-    // <h5 class="platforms">${game.plateform}</h5>
-    // <a href="/games/collection/game/${game._id}">View details</a>
-    // <p class="addToCollec" data-id="${game._id}">Add to My Collection</p>`
+  // `<h4 class="game-title">${game.title}</h4>
+  // <img src="${game.img}" alt="${game.title}-poster" class="game-poster">
+  // <h5 class="genre">${game.genres}</h5>
+  // <h5 class="platforms">${game.plateform}</h5>
+  // <a href="/games/collection/game/${game._id}">View details</a>
+  // <p class="addToCollec" data-id="${game._id}">Add to My Collection</p>`
 }
--- public/javascripts/infinitescroll.js
+++ public/javascripts/infinitescroll.js
@@ -3,44 +3,41 @@ const apiFilter = new APIHandler();
 
 let htmlBody = document.getElementById("toto"); //document.querySelector("body");
 
-
 let page = 2;
 let sending = false;
 
 // Detect when scrolled to bottom.
-htmlBody.addEventListener('scroll', async function() {
+htmlBody.addEventListener("scroll", async function () {
+  if (htmlBody.scrollTop + htmlBody.offsetHeight >= htmlBody.scrollHeight) {
+    if (sending) return; //send only one request
+    sending = true;
+    const loadnextpage = await apiFilter.getGamePage(page);
+    //console.log(loadnextpage);
+    const games = loadnextpage.data;
 
-    if (htmlBody.scrollTop + htmlBody.offsetHeight >= htmlBody.scrollHeight) {
-        if (sending) return; //send only one request
-        sending = true;
-        const loadnextpage = await apiFilter.getGamePage(page);
-        //console.log(loadnextpage);
-        const games = loadnextpage.data;
+    display(games);
 
-        display(games);
-
-        page++;
-        console.log("loading game page ", page);
-        sending = false; //request is finished
-    }
+    page++;
+    console.log("loading game page ", page);
+    sending = false; //request is finished
+  }
 });
 
 function display(games) {
-    games.forEach(game => createGameDiv(game));
+  games.forEach((game) => createGameDiv(game));
 }
 
 function createGameDiv(game) {
-    let div = document.createElement("div");
-    div.classList.add("game-container");
-    div.innerHTML = `<h4 class="game-title">${game.title}</h4>
+  let div = document.createElement("div");
+  div.classList.add("game-alignment");
+  div.innerHTML = `<h4 class="game-title">${game.title}</h4>
     <img src="${game.img}" alt="${game.title}-poster" class="game-poster">
     <h5 class="genre">${game.genres}</h5>
     <h5 class="platforms">${game.plateform}</h5>
     <a href="/games/collection/game/${game._id}">View details</a>
-    <p class="addToCollec" data-id="${game._id}">Add to My Collection</p>`
-    htmlBody.appendChild(div);
+    <p class="addToCollec" data-id="${game._id}">Add to My Collection</p>`;
+  htmlBody.appendChild(div);
 }
-
 
 /*
 let getting = false;
